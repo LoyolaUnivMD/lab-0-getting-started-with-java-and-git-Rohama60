b319854a5d48c7fc31c9bd4d172d90f0276c7d7d
Lab0 <rsmulugeta@loyola.edu>
2024-01-19 20:27:15 -0500

    Finished Lab0

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,21 @@
 //HelloWorld.java
-
+// Programmers:  Rohama Mulugeta
+// Course:  CS 212, Dr. Cui
+// Due Date: 1/19/24
+// Lab Assignment: 0
+// Problem Statement:  Output programmers’ initial
+// Input: None; there is no user input in this program
+// Output:  The initials of the programmers in ASCII art form
+// Credits:  in class excrsise
 class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
-        
+        System.out.println(" ---          --    --");
+        System.out.println("|   |        /   \\ /   \\");
+        System.out.println(" ---        /     V     \\");
+        System.out.println(" |   \\     /             \\");
+        System.out.println(" |     \\  /               \\");
+
+
     }
 }
